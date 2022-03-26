be8a86f659c15c56007eb9b09cf17392a3e3a18a
Snehel <sahilrps31@gmail.com>
2022-03-26 16:39:21 +0530

    added my experimental idea file

--- Js/main.js
+++ Js/main.js
@@ -1,38 +1,9 @@
 const search = document.getElementById('search');
 const matchList = document.getElementById('match-list');
 
-function substr(s1, s2) {
-    let m = s1.length;
-    let n = s2.length;
-    if (m > n)
-        return -1;
-    let x = Math.min(m, n);
-    for (var j = 0; j < x; j++) {
-        if (s2[j] != s1[j])
-            return -1;
-    }
-    return 1;
-}
-
 const searchStates = async searchText => {
     const res = await fetch('./data/SC.json');
     const states = await res.json();
-    // console.log(states);
-    // let arr = [], count = 0;
-    // for (let i = 0; i < states.length; i++) {
-    //     arr.push(substr(searchText, states[i].name));
-    //     arr.push(substr(searchText, states[i].abbr));
-    //     arr.push(substr(searchText, states[i].capital));
-    // }
-    // for (let i = 0; i < arr.length; i++) {
-    //     if (arr[i] === -1)
-    //         count++;
-    // }
-    // if (count === arr.length) {
-    //     matches = [];
-    //     matchList.innerHTML = '';
-    // }
-    // else
     {
         let matches = states.filter(state => {
             const regex = new RegExp(`^${searchText}`, 'gi');
